8b4f17acfc7ae247b728336627e53cdfc5d1e9a9
Sarthak <sarthak@stepapp.ai>
2020-05-27 13:10:46 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,110 @@
+require('dotenv').config();
+const { Keystone } = require('@keystonejs/keystone');
+const { PasswordAuthStrategy } = require('@keystonejs/auth-password');
+const { Text, Checkbox, Password } = require('@keystonejs/fields');
+const { GraphQLApp } = require('@keystonejs/app-graphql');
+const { AdminUIApp } = require('@keystonejs/app-admin-ui');
+const initialiseData = require('./initial-data');
+
+const { KnexAdapter: Adapter } = require('@keystonejs/adapter-knex');
+
+const PROJECT_NAME = 'stepcms';
+const adapterConfig = { knexOptions: { connection: 'postgres://localhost/stepcms' } };
+
+
+const keystone = new Keystone({
+  name: PROJECT_NAME,
+  adapter: new Adapter({
+    dropDatabase: true,
+    // knexOptions: { connection: 'postgres://localhost/step_cms' }
+    knexOptions: {
+      client: 'mysql',
+      connection: {
+        host : 'stepapp-newdev-db.cuwigx21n0pg.ap-south-1.rds.amazonaws.com',
+        user : 'admin',
+        password : 'Kde7e@yDXy',
+        database : 'stepapp_dev_db'
+      }
+    }
+  }),
+  // onConnect: process.env.CREATE_TABLES !== 'true' && initialiseData,
+});
+
+// Access control functions
+const userIsAdmin = ({ authentication: { item: user } }) => Boolean(user && user.isAdmin);
+const userOwnsItem = ({ authentication: { item: user } }) => {
+  if (!user) {
+    return false;
+  }
+
+  // Instead of a boolean, you can return a GraphQL query:
+  // https://www.keystonejs.com/api/access-control#graphqlwhere
+  return { id: user.id };
+};
+
+const userIsAdminOrOwner = auth => {
+  const isAdmin = access.userIsAdmin(auth);
+  const isOwner = access.userOwnsItem(auth);
+  return isAdmin ? isAdmin : isOwner;
+};
+
+const access = { userIsAdmin, userOwnsItem, userIsAdminOrOwner };
+
+// keystone.createList('User', {
+//   fields: {
+//     name: { type: Text },
+//     email: {
+//       type: Text,
+//       isUnique: true,
+//     },
+//     isAdmin: {
+//       type: Checkbox,
+//       // Field-level access controls
+//       // Here, we set more restrictive field access so a non-admin cannot make themselves admin.
+//       access: {
+//         update: access.userIsAdmin,
+//       },
+//     },
+//     password: {
+//       type: Password,
+//     },
+//   },
+//   // List-level access controls
+//   access: {
+//     read: access.userIsAdminOrOwner,
+//     update: access.userIsAdminOrOwner,
+//     create: access.userIsAdmin,
+//     delete: access.userIsAdmin,
+//     auth: true,
+//   },
+// });
+
+
+keystone.createList('Board', {
+  fields: {
+    id : { type: Number },
+    board_name: { type: Text},
+   
+  },
+
+  labelField: 'id',
+  
+});
+
+
+
+// const authStrategy = keystone.createAuthStrategy({
+//   type: PasswordAuthStrategy,
+//   list: 'User',
+// });
+
+module.exports = {
+  keystone,
+  apps: [
+    new GraphQLApp(),
+    new AdminUIApp({
+      enableDefaultRoute: true,
+      // authStrategy,
+    }),
+  ],
+};
